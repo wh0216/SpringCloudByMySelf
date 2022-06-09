a5b7ef0b8badb546205038b13ca9782e53627d86
wh0216 <1520577322@qq.com>
2022-06-09 22:11:43 +0800

    测试接口，安装云数据库

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymetnMain8001.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymetnMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymetnMain8001.class,args);
+    }
+}
